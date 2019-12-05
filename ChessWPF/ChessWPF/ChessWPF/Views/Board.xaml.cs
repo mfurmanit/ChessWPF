0d4953846269d73f4fcff91be2dbb3606557426e
mfurmanit <maciek.furman.user@gmail.com>
2019-12-05 20:28:58 +0100

    CH-1, CH-2, CH-3 - Create basic chess board. Draw black and white tiles on the board.

---
+++ ChessWPF/ChessWPF/ChessWPF/Views/Board.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ChessWPF.Views
+{
+    public partial class Board : UserControl
+    {
+        public Board()
+        {
+            InitializeComponent();
+            DrawTilesOnBoard();
+        }
+
+        private void DrawTilesOnBoard()
+        {
+            var BLACK_TILE_COLOR = Brushes.LightSteelBlue;
+            var WHITE_TILE_COLOR = Brushes.White;
+            var BORDER_TILE_COLOR = Brushes.Black;
+
+            int tilesCount = 64;
+            bool isWhite = false;
+
+            for (int tileIndex = 1; tileIndex <= tilesCount; tileIndex++)
+            {
+                Rectangle rect = new Rectangle
+                {
+                    Name = "TILE" + tileIndex,
+                    Stroke = BORDER_TILE_COLOR,
+                    Fill = isWhite ? WHITE_TILE_COLOR : BLACK_TILE_COLOR
+                };
+
+                UniformGrid.Children.Add(rect);
+
+                if (tileIndex % 8 != 0)
+                    isWhite = !isWhite;
+            }
+        }
+
+    }
+}
---
+++ ChessWPF/ChessWPF/ChessWPFTests/BoardTests.cs
@@ -0,0 +1,17 @@
+using ChessWPF.Views;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessWPFTests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void Create_ChessBoard_Success()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
