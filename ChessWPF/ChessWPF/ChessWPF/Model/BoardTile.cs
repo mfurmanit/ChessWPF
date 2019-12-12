3e3f6056875190ef26c940bdfb252cbbe1ca9198
karolinaJot <amicus.karo@gmail.com>
2019-12-12 22:08:51 +0100

    CH-39 add BindableBase (utility for MVVM)

--- ChessWPF/ChessWPF/ChessWPF/Model/BoardTile.cs
+++ ChessWPF/ChessWPF/ChessWPF/Model/BoardTile.cs
@@ -1,96 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media;
+﻿using System.Windows.Media;
 
 namespace ChessWPF.Model
 {
-    class BoardTile : INotifyPropertyChanged
-    {
-        private int index;
-        private SolidColorBrush background;
-        private SolidColorBrush stroke;
-        private bool isActive;
-        private BoardFigure figure;
+	class BoardTile : Utils.BindableBase
+	{
+		private int index;
+		private SolidColorBrush background;
+		private SolidColorBrush stroke;
+		private bool isActive;
+		private BoardFigure figure;
 
-        public int Index
-        {
-            get
-            {
-                return index;
-            }
-            set
-            {
-                index = value;
-                OnPropertyChanged("Index");
-            }
-        }
+		public int Index
+		{
+			get => index;
+			set => SetProperty(ref index, value);
+		}
 
-        public SolidColorBrush Background
-        {
-            get
-            {
-                return background;
-            }
-            set
-            {
-                background = value;
-                OnPropertyChanged("Background");
-            }
-        }
+		public SolidColorBrush Background
+		{
+			get => background;
+			set => SetProperty(ref background, value);
+		}
 
-        public SolidColorBrush Stroke
-        {
-            get
-            {
-                return stroke;
-            }
-            set
-            {
-                stroke = value;
-                OnPropertyChanged("Stroke");
-            }
-        }
+		public SolidColorBrush Stroke
+		{
+			get => stroke;
+			set => SetProperty(ref stroke, value);
+		}
 
-        public bool IsActive
-        {
-            get
-            {
-                return isActive;
-            }
-            set
-            {
-                isActive = value;
-                OnPropertyChanged("IsActive");
-            }
-        }
+		public bool IsActive
+		{
+			get => isActive;
+			set => SetProperty(ref isActive, value);
+		}
 
-        public BoardFigure Figure
-        {
-            get
-            {
-                return figure;
-            }
-            set
-            {
-                figure = value;
-                OnPropertyChanged("Figure");
-            }
-        }
-
-        #region INotifyPropertyChanged Members  
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-        #endregion
-    }
+		public BoardFigure Figure
+		{
+			get => figure;
+			set => SetProperty(ref figure, value);
+		}
+	}
 }
