5171ae09fc138890a8a6d5ad03ed068d8ee8c928
mfurmanit <maciek.furman.user@gmail.com>
2020-01-03 19:50:37 +0100

    CH-19 - Create some simple unit tests.

---
+++ ChessWPF/ChessWPF/ChessWPFTests/FiguresInitializationTests.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using ChessWPF;
+using ChessWPF.Model.Constants;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessWPFTests
+{
+    [TestClass]
+    public class FiguresInitializationTests
+    {
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesFigures()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var actualCount = mainWindowModel.BoardViewModel.BoardFigures.Count;
+            var expectedCount = 32;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesDarkPawns()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Pawn && tile.Figure.Color == FigureColor.Dark);
+            var actualCount = resultList.Count;
+            var expectedCount = 8;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesWhitePawns()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Pawn && tile.Figure.Color == FigureColor.White);
+            var actualCount = resultList.Count;
+            var expectedCount = 8;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesDarkRooks()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Rook && tile.Figure.Color == FigureColor.Dark);
+            var actualCount = resultList.Count;
+            var expectedCount = 2;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesWhiteRooks()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Rook && tile.Figure.Color == FigureColor.White);
+            var actualCount = resultList.Count;
+            var expectedCount = 2;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesDarkKnights()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Knight && tile.Figure.Color == FigureColor.Dark);
+            var actualCount = resultList.Count;
+            var expectedCount = 2;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesWhiteKnights()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Knight && tile.Figure.Color == FigureColor.White);
+            var actualCount = resultList.Count;
+            var expectedCount = 2;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesDarkBishops()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Bishop && tile.Figure.Color == FigureColor.Dark);
+            var actualCount = resultList.Count;
+            var expectedCount = 2;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesWhiteBishops()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Bishop && tile.Figure.Color == FigureColor.White);
+            var actualCount = resultList.Count;
+            var expectedCount = 2;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesDarkQueen()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Queen && tile.Figure.Color == FigureColor.Dark);
+            var actualCount = resultList.Count;
+            var expectedCount = 1;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesWhiteQueen()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.Queen && tile.Figure.Color == FigureColor.White);
+            var actualCount = resultList.Count;
+            var expectedCount = 1;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesDarkKing()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.King && tile.Figure.Color == FigureColor.Dark);
+            var actualCount = resultList.Count;
+            var expectedCount = 1;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod]
+        public void Test_MainWindowModel_Initialization_CreatesWhiteKing()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var resultList = mainWindowModel.BoardViewModel.BoardTiles
+                .FindAll(tile => tile.Figure != null && tile.Figure.Type == FigureType.King&& tile.Figure.Color == FigureColor.White);
+            var actualCount = resultList.Count;
+            var expectedCount = 1;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+    }
+}
---
+++ ChessWPF/ChessWPF/ChessWPFTests/FiguresMovesTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChessWPF;
+using System.Threading;
+
+namespace ChessWPFTests
+{
+    /// <summary>
+    /// Summary description for FiguresMovesTests
+    /// </summary>
+    [TestClass]
+    public class FiguresMovesTests
+    {
+
+        [TestMethod]
+        public void Test_WhitePawn_Move_MovesOnePosition()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var basePosition = 48;
+            var newPosition = 40;
+            var pawn = mainWindowModel.BoardViewModel.BoardTiles[basePosition];
+            var someTile = mainWindowModel.BoardViewModel.BoardTiles[newPosition];
+            var pawnFigure = pawn.Figure;
+
+            //Act
+            pawn.ClickCommand.Execute(pawn);
+            someTile.ClickCommand.Execute(someTile);
+
+            //Assert
+            Assert.AreEqual(pawnFigure.TileIndex, newPosition);
+            Assert.AreEqual(someTile.Figure, pawnFigure);
+            Assert.AreEqual(pawn.Figure, null);
+        }
+
+        [TestMethod]
+        public void Test_WhitePawn_Move_MovesTwoPositions()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var basePosition = 48;
+            var newPosition = 32;
+            var pawn = mainWindowModel.BoardViewModel.BoardTiles[basePosition];
+            var someTile = mainWindowModel.BoardViewModel.BoardTiles[newPosition];
+            var pawnFigure = pawn.Figure;
+
+            //Act
+            pawn.ClickCommand.Execute(pawn);
+            someTile.ClickCommand.Execute(someTile);
+
+            //Assert
+            Assert.AreEqual(pawnFigure.TileIndex, newPosition);
+            Assert.AreEqual(someTile.Figure, pawnFigure);
+            Assert.AreEqual(pawn.Figure, null);
+        }
+
+        [TestMethod]
+        public void Test_DarkPawn_Move_CantMoveBeforeWhite()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            var basePosition = 8;
+            var newPosition = 16;
+            var pawn = mainWindowModel.BoardViewModel.BoardTiles[basePosition];
+            var someTile = mainWindowModel.BoardViewModel.BoardTiles[newPosition];
+            var pawnFigure = pawn.Figure;
+
+            //Act
+            pawn.ClickCommand.Execute(pawn);
+            someTile.ClickCommand.Execute(someTile);
+
+            //Assert
+            Assert.AreEqual(pawnFigure.TileIndex, basePosition);
+            Assert.AreEqual(someTile.Figure, null);
+            Assert.AreEqual(pawn.Figure, pawnFigure);
+        }
+
+        [TestMethod]
+        public void Test_DarkPawn_Move_MovesOnePosition()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            mainWindowModel.StartViewModel.ClickCommand.Execute(mainWindowModel.StartViewModel);
+
+            //White makes first move
+            var baseWhitePosition = 48;
+            var newWhitePosition = 40;
+            var whitePawn = mainWindowModel.BoardViewModel.BoardTiles[baseWhitePosition];
+            var someWhiteTile = mainWindowModel.BoardViewModel.BoardTiles[newWhitePosition];
+            var whitePawnFigure = whitePawn.Figure;
+            whitePawn.ClickCommand.Execute(whitePawn);
+            someWhiteTile.ClickCommand.Execute(someWhiteTile);
+
+            Assert.AreEqual(mainWindowModel.GameViewModel.ActualPlayer.SelectedColor, ChessWPF.Model.Constants.FigureColor.Dark);
+
+            var basePosition = 8;
+            var newPosition = 16;
+            var pawn = mainWindowModel.BoardViewModel.BoardTiles[basePosition];
+            var someTile = mainWindowModel.BoardViewModel.BoardTiles[newPosition];
+            var pawnFigure = pawn.Figure;
+
+            //Act
+            pawn.ClickCommand.Execute(pawn);
+            someTile.ClickCommand.Execute(someTile);
+
+            //Assert
+            Assert.AreEqual(pawnFigure.TileIndex, newPosition);
+            Assert.AreEqual(someTile.Figure, pawnFigure);
+            Assert.AreEqual(pawn.Figure, null);
+        }
+
+        [TestMethod]
+        public void Test_DarkPawn_Move_MovesTwoPositions()
+        {
+            //Arrange
+            MainWindowModel mainWindowModel = new MainWindowModel();
+            mainWindowModel.StartViewModel.ClickCommand.Execute(mainWindowModel.StartViewModel);
+
+            //White makes first move
+            var baseWhitePosition = 48;
+            var newWhitePosition = 40;
+            var whitePawn = mainWindowModel.BoardViewModel.BoardTiles[baseWhitePosition];
+            var someWhiteTile = mainWindowModel.BoardViewModel.BoardTiles[newWhitePosition];
+            var whitePawnFigure = whitePawn.Figure;
+            whitePawn.ClickCommand.Execute(whitePawn);
+            someWhiteTile.ClickCommand.Execute(someWhiteTile);
+
+            Assert.AreEqual(mainWindowModel.GameViewModel.ActualPlayer.SelectedColor, ChessWPF.Model.Constants.FigureColor.Dark);
+
+            var basePosition = 8;
+            var newPosition = 24;
+            var pawn = mainWindowModel.BoardViewModel.BoardTiles[basePosition];
+            var someTile = mainWindowModel.BoardViewModel.BoardTiles[newPosition];
+            var pawnFigure = pawn.Figure;
+
+            //Act
+            pawn.ClickCommand.Execute(pawn);
+            someTile.ClickCommand.Execute(someTile);
+
+            //Assert
+            Assert.AreEqual(pawnFigure.TileIndex, newPosition);
+            Assert.AreEqual(someTile.Figure, pawnFigure);
+            Assert.AreEqual(pawn.Figure, null);
+        }
+    }
+}
